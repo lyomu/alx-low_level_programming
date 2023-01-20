082ff8f9808abf6f76daaf7c20045744d6afd1e6
Gideon Lyomu <65831672+lyomu@users.noreply.github.com>
2023-01-21 01:38:39 +0300

    New corrections

---
+++ 0x18-dynamic_libraries/MYLIB.h
@@ -0,0 +1,12 @@
+#ifndef MAINL_H
+#define MAINL_H
+
+
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "MYLIB.h"
+
+int main()
+{
+	printf("%d", div(3,6));
+	return (0);
+}
